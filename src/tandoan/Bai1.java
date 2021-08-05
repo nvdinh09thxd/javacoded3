56afdb2f84f00d69826eff5ad6c2c073b205697b
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-08-05 21:16:53 +0700

    format code

--- src/tandoan/Bai1.java
+++ src/tandoan/Bai1.java
@@ -3,32 +3,32 @@ package tandoan;
 import java.util.Scanner;
 
 public class Bai1 {
-	public static void dem(int n){
-		int chiahet3=0;
-		int chiahet5=0;
+	public static void dem(int n) {
+		int chiahet3 = 0;
+		int chiahet5 = 0;
 		for (int i = 0; i <= n; i++) {
-			if (i%3==0) {
+			if (i % 3 == 0) {
 				chiahet3++;
-				
+
 			}
-			if (i%5==0) {
+			if (i % 5 == 0) {
 				chiahet5++;
 			}
-			
+
 		}
-		System.out.println("có "+chiahet3 + " số chia hết cho 3");
-		System.out.println("có "+chiahet5 + " số chia hết cho 5");
+		System.out.println("có " + chiahet3 + " số chia hết cho 3");
+		System.out.println("có " + chiahet5 + " số chia hết cho 5");
 	}
+
 	public static void main(String[] args) {
-		Scanner input= new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int n;
 		do {
 			System.out.println("nhập n:");
-			n= input.nextInt();
-		} while (n<10 || n>100);
+			n = input.nextInt();
+		} while (n < 10 || n > 100);
 		dem(n);
-		
+
 	}
-	
 
 }
--- src/tandoan/Bai3.java
+++ src/tandoan/Bai3.java
@@ -3,85 +3,120 @@ package tandoan;
 import java.util.Scanner;
 
 public class Bai3 {
-	public static String docHangDonVi(int n){
-		switch(n){
-		case 0: return "";
-		case 1: return "một";
-		case 2: return "hai";
-		case 3: return "ba";
-		case 4: return "bốn";
-		case 5: return "năm";
-		case 6: return "sáu";
-		case 7: return "bảy";
-		case 8: return "tám";
-		case 9: return "chín";
+	public static String docHangDonVi(int n) {
+		switch (n) {
+		case 0:
+			return "";
+		case 1:
+			return "một";
+		case 2:
+			return "hai";
+		case 3:
+			return "ba";
+		case 4:
+			return "bốn";
+		case 5:
+			return "năm";
+		case 6:
+			return "sáu";
+		case 7:
+			return "bảy";
+		case 8:
+			return "tám";
+		case 9:
+			return "chín";
 		}
 		return "";
-	}
-	public static String docHangChuc(int n){
-		switch(n){
-		case 0: return "linh";
-		case 1: return "mười";
-		case 2: return "hai mươi";
-		case 3: return "ba mươi";
-		case 4: return "bốn mươi";
-		case 5: return "năm mươi";
-		case 6: return "sáu mươi";
-		case 7: return "bảy mươi";
-		case 8: return "tám mươi";
-		case 9: return "chín mươi";
-		}
-		return "";
-	}
-	public static String docHangTram(int n){
-		switch(n){
-		case 0: return "";
-		case 1: return "một trăm";
-		case 2: return "hai trăm";
-		case 3: return "ba trăm";
-		case 4: return "bốn trăm";
-		case 5: return "năm trăm";
-		case 6: return "sáu trăm";
-		case 7: return "bảy trăm";
-		case 8: return "tám trăm";
-		case 9: return "chín trăm";
-		}
-		return "";
-	}
-	public static String readNumber(int n){
-		if(n==0) return "Không";
-		int du;
-		
-		String s1="",s2="",s3="";
-		int dem=0;
-		while(n!=0){
-			du=n%10;
-			if(dem==0)
-			s1=docHangDonVi(du);
-			else if(dem==1)
-				s2=docHangChuc(du);
-			else s3=docHangTram(du);
-			n=n/10;
-			dem++;
-		}
-		if(s1=="" && s2=="linh")
-			s1=s2="";
-		return (s3+" "+s2+" "+s1);
-	}
-	public static int inputNumber(){
-		Scanner sc=new Scanner(System.in);
-		int n;
-		do{
-			System.out.println("Nhập n :");
-			n=sc.nextInt();
-		}
-		while(n<0||n>=1000);
-		return n;
-	}
-	public static void main(String[] args) {
-		int n=inputNumber();
-		System.out.println(readNumber(n));
 	}
 
+	public static String docHangChuc(int n) {
+		switch (n) {
+		case 0:
+			return "linh";
+		case 1:
+			return "mười";
+		case 2:
+			return "hai mươi";
+		case 3:
+			return "ba mươi";
+		case 4:
+			return "bốn mươi";
+		case 5:
+			return "năm mươi";
+		case 6:
+			return "sáu mươi";
+		case 7:
+			return "bảy mươi";
+		case 8:
+			return "tám mươi";
+		case 9:
+			return "chín mươi";
+		}
+		return "";
+	}
+
+	public static String docHangTram(int n) {
+		switch (n) {
+		case 0:
+			return "";
+		case 1:
+			return "một trăm";
+		case 2:
+			return "hai trăm";
+		case 3:
+			return "ba trăm";
+		case 4:
+			return "bốn trăm";
+		case 5:
+			return "năm trăm";
+		case 6:
+			return "sáu trăm";
+		case 7:
+			return "bảy trăm";
+		case 8:
+			return "tám trăm";
+		case 9:
+			return "chín trăm";
+		}
+		return "";
+	}
+
+	public static String readNumber(int n) {
+		if (n == 0)
+			return "Không";
+		int du;
+
+		String s1 = "", s2 = "", s3 = "";
+		int dem = 0;
+		while (n != 0) {
+			du = n % 10;
+			if (dem == 0)
+				s1 = docHangDonVi(du);
+			else if (dem == 1)
+				s2 = docHangChuc(du);
+			else
+				s3 = docHangTram(du);
+			n = n / 10;
+			dem++;
+		}
+		if (s1 == "" && s2 == "linh")
+			s1 = s2 = "";
+		return (s3 + " " + s2 + " " + s1);
+	}
+
+	public static int inputNumber() {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		do {
+			System.out.println("Nhập n :");
+			n = sc.nextInt();
+		} while (n < 0 || n >= 1000);
+		return n;
+	}
+
+	public static void main(String[] args) {
+		int n = inputNumber();
+		System.out.println(readNumber(n));
+	}
 
 }
